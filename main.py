82b3d757d1bca889282a0164cd4e7cda62402b5e
Bogdan <prodanov1540@gmail.com>
2022-03-18 00:12:08 +0200

    Added light, material classes and progress bar

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from point import Point
 from sphere import Sphere
 from scene import Scene
 from engine import RenderEngine
+from light import Light
+from material import Material
 
 
 def main():
     WIDTH = 320
     HEIGHT = 200
     camera = Vector(0, 0, -1)
-    objects = [Sphere(Point(0, 0, 0), 0.5, Color.from_hex("#FF0000"))]
-    scene = Scene(camera, objects, WIDTH, HEIGHT)
+    objects = [Sphere(Point(0, 0, 0), 0.5, Material(Color.from_hex("#FF0000")))]
+    lights = [Light(Point(1.5, -0.5, -10.0), Color.from_hex("#FFFFFF"))]
+    scene = Scene(camera, objects, lights, WIDTH, HEIGHT)
     engine = RenderEngine()
     image = engine.render(scene)
 
--- scene.py
+++ scene.py
@@ -1,7 +1,8 @@
 class Scene:
     """Scene has all the information needed for the ray tracing engine"""
-    def __init__(self, camera, objects, width, height):
+    def __init__(self, camera, objects, lights, width, height):
         self.camera = camera
         self.objects = objects
+        self.lights = lights
         self.width = width
         self.height = height
